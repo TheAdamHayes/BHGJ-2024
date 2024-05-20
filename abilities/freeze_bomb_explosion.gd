9551872cdde13e97e9e51617287aeaf5bfc9db86
TheAdamHayes <adam.hayes@aheadmusicproduction.com>
2024-05-20 15:41:23 +0100

    Final Touches 3

--- abilities/freeze_bomb_explosion.gd
+++ abilities/freeze_bomb_explosion.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 var radius: int = 40
-var damage: int = 2
+var damage: int = 0
 @onready var life_timer: Timer = $LifeTimer
 
 # Called when the node enters the scene tree for the first time.
@@ -14,11 +14,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	area_entered.connect(on_explosion_hit)
+	body_entered.connect(on_explosion_hit)
+	life_timer.timeout.connect(queue_free)
+	Events.wave_ended.connect(queue_free)
 
-func _draw():
-	await $AnimatedSprite2D.animation_finished
-	draw_circle(Vector2.ZERO, radius, Color.LIGHT_BLUE)
 
 func on_explosion_hit(collider: Node2D):
 	if collider.has_method("take_damage"):
