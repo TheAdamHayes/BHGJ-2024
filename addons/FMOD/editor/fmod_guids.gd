ef427556e80102d92480c696dd62131c0654eee6
cody967 <cody.mos@sbcglobal.net>
2024-05-20 01:09:37 -0700

    Updated Music/Audio Manager

---
+++ addons/FMOD/editor/fmod_guids.gd
@@ -0,0 +1,41 @@
+#
+   # fmod_guids.gd - FMOD Studio API
+
+   # Generated GUIDs for project 'BHGJ_2024_.fspro'
+#
+
+class_name FMODGuids
+
+class Events:
+	const MUSIC_LEVEL1_BATTLE = "{7cc75bb5-6913-4415-9e50-44bee1d70846}"
+	const MUSIC_MAIN_MENU = "{dee76801-3250-4418-beba-f019b9cc1cd5}"
+	const SFX_ENEMY_DEATH = "{438c883a-75b8-4654-a510-fb4bc68525a7}"
+	const SFX_ENEMY_SIMPLESHOT = "{b7caf06b-fab2-4fd9-86b9-2c31d920a113}"
+	const SFX_ENVIRONMENT_LEVELBEGIN = "{291a8c5f-8d67-4167-909c-fbdab6566e93}"
+	const SFX_ENVIRONMENT_LEVELCOMPLETE = "{918ae3f1-0795-47b6-a18f-396758142a5c}"
+	const SFX_ENVIRONMENT_LOWHEALTH = "{993d762f-f2b9-4aba-a34c-f46dbbe0dbe1}"
+	const SFX_ENVIRONMENT_PLAYERDEATH = "{d8e60467-7e8b-4323-a744-45dec16eb27a}"
+	const SFX_ENVIRONMENT_TURRETADDED = "{934c582c-b434-41d7-89cf-ed84dca506ad}"
+	const SFX_ENVIRONMENT_WAVECOMPLETE = "{9f4016bc-f21d-4adc-8a30-0546361a0fb5}"
+	const SFX_PLAYER_MOVEMENT = "{49717e3c-b2de-47ec-a66f-cd4e79571d0f}"
+	const SFX_PLAYER_SIMPLESHOT = "{aee7ce83-e5c0-41bd-ade7-8f25b3f0ac59}"
+	const SFX_PLAYER_TIMEFREEZEBOMB = "{63cca832-a305-4ec0-931e-8bea754f477a}"
+	const SFX_PLAYER_TRIPLESHOT = "{2ed29855-306c-40fa-bf6e-8a9b70bd1658}"
+	const SFX_PLAYER_TURRETSHOT = "{5a1c03f0-459a-46d4-92b4-cc0698be12e3}"
+	const SFX_UI_HOVER = "{70111858-2160-41f4-bcfa-bbf559687512}"
+	const SFX_UI_SELECTION = "{8b54b777-8f67-492c-bf7d-b892ee10dbc9}"
+	const SFX_UI_UPGRADEMENUSELECTION = "{173a8ce7-6c74-483d-baeb-d8fdaf5d83ba}"
+
+class Busses:
+	const MASTER_BUS = "{df400856-a9a1-4983-826f-9840842e7364}"
+	const MASTER = "{a1802321-d93a-4f30-9b71-15371ed2549c}"
+	const MASTER_MUSIC = "{37d4e12f-b221-4e9a-9e90-0729816aa52a}"
+	const MASTER_SFX = "{997d4d3a-4a2c-4b23-a05c-3a6985a99664}"
+	const REVERB = "{2b4cc931-6eee-4dbb-a1c2-33bd15dcd67e}"
+
+class Banks:
+	const MASTER = "{df603525-ade5-4b4c-9a41-4a58ed07e32e}"
+
+class Parameters:
+	const TIMEFREEZEBOMB_STATUS = "{b9bdf2b2-1b67-4d31-a45e-6073ab73bb96}"
+
--- autoload/audio_manager.gd
+++ autoload/audio_manager.gd
@@ -1,7 +1,9 @@
 extends Node
 
 var freezeBombInstance: EventInstance
-
+var masterBus: Bus
+var sfxBus: Bus
+var musicBus: Bus
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,6 +12,10 @@ func _ready():
 	Events.connect("turret_added", onTurretAdd)
 	
 	freezeBombInstance = FMODRuntime.create_instance_path("event:/TestEvent")
+	
+	masterBus = FMODStudioModule.get_studio_system().get_bus_by_id(FMODGuids.Busses.MASTER)
+	sfxBus = FMODStudioModule.get_studio_system().get_bus_by_id(FMODGuids.Busses.MASTER_SFX)
+	musicBus = FMODStudioModule.get_studio_system().get_bus_by_id(FMODGuids.Busses.MASTER_MUSIC)
 
 func onWaveEnd():
 	FMODRuntime.play_one_shot_path("event:/SFX/Environment/WaveComplete")
@@ -18,8 +24,11 @@ func onTurretAdd():
 	FMODRuntime.play_one_shot_path("event:/SFX/Environment/TurretAdded")
 
 func setFreezeBombStatus(status):
-	if (status == "traveling"):
-		freezeBombInstance.start()
-	elif (status == "hit"):
-		freezeBombInstance.set_parameter_by_name_with_label("freezeBombStatus", "hit", false)
 	print_debug(status)
+
+func _process(delta):
+	
+	#Sets the volume levels for each bus, expand if needed. Vals between 0.0 amd 1.0, 0.5 is default
+	masterBus.set_volume(0.5)
+	sfxBus.set_volume(0.5)
+	musicBus.set_volume(0.5)
